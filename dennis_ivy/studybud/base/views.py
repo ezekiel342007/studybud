e98af362626fd12f6374f14fe1261ecec67b4b06
ezekiel342007 <ezekiel342007@gmail.com>
2025-06-12 18:57:48 +0100

    learnt login and authenticated

--- dennis_ivy/studybud/base/views.py
+++ dennis_ivy/studybud/base/views.py
@@ -1,4 +1,8 @@
+from django.db.models import Q
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
 from .models import Room, Topic
@@ -6,16 +10,36 @@ from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-    {"id": 1, "name": "Let's learn python!"},
-    {"id": 2, "name": "Design with me"},
-    {"id": 3, "name": "Frontend developers"},
-]
+def login_page(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or password does not exist")
+    context = {}
+    return render(request, "base/login_register.html", context)
+
 
 def home(request: HttpRequest) -> HttpResponse:
-    rooms = Room.objects.all()
+    q = request.GET.get("q") if request.GET.get("q") is not None else ""
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    room_count = rooms.count()
     topics = Topic.objects.all
-    context = {"rooms": rooms}
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
